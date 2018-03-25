82101191c04bac6895a740fa5c9206db94fe6256
qaashu007 <qaashu007@gmail.com>
2018-03-25 15:46:30 +0530

    sample file as config.json

---
+++ conf.sample.js
@@ -0,0 +1,18 @@
+{
+  "siteURL": "https://quantra-preprod.quantinsti.com/community",
+  "loginEmail": "quantra+interview@quantinsti.com",
+  "loginPass": "test@123",
+  "postTitle": "First Post on Quantra",
+  "postDescription": "These are the full e2e automated tests that actually run inside a browser.",
+  "dependencies": {
+    "java-script": "",
+    "protractor": "^5.0.0",
+    "webdriver-manager": "10.2.8"
+  },
+  "devDependencies": {},
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}
