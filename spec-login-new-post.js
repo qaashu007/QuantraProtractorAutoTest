a323366b3d8a2a0d681f17ed30f6125fc2dd2c7f
qaashu007 <qaashu007@gmail.com>
2018-03-26 13:41:19 +0530

    update new post test case

--- spec-login-new-post.js
+++ spec-login-new-post.js
@@ -3,36 +3,40 @@ var config = require('./config.json');
 
 describe ("Protractor login & new post test", function(){
 	
-	
+    
     it('should able to login', function(){
 		browser.ignoreSynchronization = true;			 
 		browser.get(config.siteURL);                                         //launch url
-		browser.sleep(9000);
+		browser.sleep(6000);
 		browser.waitForAngular();
        // element(by.css('.openLogin.inner-header-link.ng-binding')).click();  
 	    element(by.xpath('//a[contains(text(), "Login")]')).click();
-		browser.sleep(4000);                                         
+		browser.sleep(3000);                                         
 		element(by.model('login.email')).sendKeys(config.loginEmail);        //enter login email
 		element(by.model('login.password')).sendKeys(config.loginPass);      //enter login password
 		element(by.xpath('//input[@type="submit"]')).click();                //click login button
-		browser.sleep(9000);
+		browser.sleep(2000);
 		browser.waitForAngular();
 		expect(element(by.css('.openLogin.inner-header-link.ng-binding')).isPresent()).toBe(false);   //verify loggedin page
     });
 	
 	it('should get an error during new post',function(){
 		browser.ignoreSynchronization = true;
-		browser.sleep(3000);
+		browser.sleep(5000);
 		browser.waitForAngular();
-		element.all(by.css('.follow.not-following.ng-scope')).get(0).click();  //upvote first post 
+		element.all(by.css('.question-title-common.click-for-more')).get(0).click();  //upvote first post 
+		browser.sleep(3000);
+		element(by.css('.btn.btn-white.btn-prv')).click();
 		browser.sleep(1000);
-		element(by.css('.btn.new-post-btn.ng-binding')).click();               //click new post button
+		element(by.css('.btn.new-post-btn.ng-binding')).click();                     //click new post button
 		browser.sleep(5000);
 		element(by.model('ngDialogData.communityCntrlObj.title')).sendKeys(config.postTitle);   //enter post title
-		element(by.css('.cke_editable.cke_editable_themed.cke_contents_ltr.cke_show_borders')).sendKeys(config.postDescription);  //enter description
+		browser.sleep(2000);
+		element(by.xpath('//*[@id="cke_1_contents"]/iframe')).element(by.css('body.cke_editable.cke_editable_themed.cke_contents_ltr.cke_show_borders')).sendKeys(config.postDescription);  //enter description
+		browser.sleep(3000);
 		element.all(by.css('.tag.addable.clickable.ng-binding.ng-scope.unselected')).get(0).click();    //click tag
 		element(by.css('.btn.btn-primary.post-save.ng-binding')).click();      //click post button
-		browser.sleep(1000);
+		browser.sleep(3000);
 		browser.waitForAngular();
 		expect(element(by.xpath('//div[contains(text(), "Recaptcha required")]')).isPresent()).toBe(true);   //verify error message
 	});
